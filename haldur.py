609b976a0a34eb8d7cfd7daf179bd2782a83d3a8
mjanisk <44684233+mjanisk@users.noreply.github.com>
2018-11-14 21:08:18 +0200

    Update haldur.py

--- haldur.py
+++ haldur.py
@@ -6,7 +6,7 @@ import shutil
 def valik(): #easygui funktsioon
     msg ="Mida sa teha soovid?"
     title = "Failihaldur"
-    choices = ["Sorteeri žanri järgi", "Failide ümbernimetamine", "3", "4"] #valik, mida kasutaja taha programmiga teha
+    choices = ["Sorteeri žanri järgi", "Failide ümbernimetamine", "Failide ümbernimetamine tunnuse järgi", "4"] #valik, mida kasutaja taha programmiga teha
     global choice
     choice = easygui.choicebox(msg, title, choices)
 
@@ -29,8 +29,35 @@ def nimeta(kaust, positsioon, liide):
             os.rename(kaust3 + aa[0] + vana_nimi, kaust3 + aa[0] + liide + nime_list[0] + '.' + nime_list[1])
         else:
             os.rename(kaust3 + aa[0] + vana_nimi, kaust3 + aa[0] + nime_list[0] + liide + '.' + nime_list[1])
-
+            
+def sorteerinimeta(kaust4, valik2):
+    if valik2 == "Žanr":
+        list1 = []
+        for i in os.walk(kaust4):
+            for j in i[2]:
+                properties1 = propsys.SHGetPropertyStoreFromParsingName(kaust4+aa[0]+j)
+                title1 = properties1.GetValue(pscon.PKEY_Music_Genre)
+                list1.append([j,title1.GetValue()[0]])
+        print(list1)
+    elif valik2 == "Album":
+        list1 = []
+        for i in os.walk(kaust4):
+            for j in i[2]:
+                properties1 = propsys.SHGetPropertyStoreFromParsingName(kaust4+aa[0]+j)
+                title1 = properties1.GetValue(pscon.PKEY_Music_AlbumTitle)
+                list1.append([j,title1.GetValue()])
+        print(list1)
+    elif valik2 == "Faililõpp":
+        list1 = []
+        for i in os.walk(kaust4):
+            for j in i[2]:
+                properties1 = propsys.SHGetPropertyStoreFromParsingName(kaust4+aa[0]+j)
+                title1 = properties1.GetValue(pscon.PKEY_FileName)
+                list1.append([j,title1.GetValue().split(".")[::-1][0]])
+        print(list1)
+                
 valik()
+
 a = ["\\"]; aa=(''.join(a)) #escape cancer
 if choice == "Sorteeri žanri järgi":
     kaust = easygui.diropenbox(msg="Vali kaust, kus soovid sorteerimist läbi viia", title=None, default=None) 
@@ -44,3 +71,11 @@ elif choice == "Failide ümbernimetamine":
     var2 = easygui.buttonbox("Kas soovid midagi lisada ette või taha?: ", "", ["Ette","Taha"])
     liide = easygui.enterbox('Mida soovid lisada failinimedele?')
     nimeta(kaust3, var2, liide)
+
+elif choice == "Failide ümbernimetamine tunnuse järgi":
+    kaust4 = easygui.diropenbox(msg="Vali kaust, kus soovid ümbernimetamist tunnuse järgi läbi viia", title=None, default=None)
+    valik2 = easygui.buttonbox("Millise tunnuse järgi soovid faili ümber nimetada?", "", ["Žanr","Album","Faililõpp"])
+    sorteerinimeta(kaust4, valik2)
+    
+
+    
